c885db63f4a804da16878cd9663309ea7ee3cf53
Diego Paralon <diegoparalon@hotmail.com>
2022-03-24 22:07:00 +0000

    Adicionando navegação simples

---
+++ src/Albums.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+
+
+const Albums = () => {
+
+ return (
+   <h1>Albums</h1>
+ )
+}
+
+export default Albums;
--- src/App.js
+++ src/App.js
@@ -1,14 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import Albums from './Albums';
 import Counter from './Counter'
+import Users from './Users'
+
+const defaultPage = 'albums'
+
+const pages = {
+  albums: {
+    text: 'Albuns',
+    component: Albums,
+  },
+  counter: {
+    text: 'Contador',
+    component: Counter,
+  },
+}
 
 
 
 function App() {
- return (
-   <>
-    <Counter/>
-   </>
- )
+const [page, setPage] = useState(defaultPage)
+
+const handleChangePage = page => {
+  setPage(page)
+}
+
+const Page = pages[page].component
+
+const pageNames = Object.keys(pages)
+
+return (
+  <>
+  {
+    pageNames.map(page => <button onClick={() => handleChangePage(page)}>{pages[page].text}</button>)
+  }
+
+  {page && <Page />}
+  </>
+)
+
 }
 
 export default App;
